1e16e4348d271cda403b3d93ef0d358e8b725d04
shahadat khan <khanshahadat459@gmail.com>
2024-04-04 08:03:41 +0530

    day6

---
+++ src/test/java/day6/JSONSchemaValidation.java
@@ -0,0 +1,30 @@
+package day6;
+
+import org.testng.annotations.Test;
+
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+//json ---> jsonschema converter
+//  https://jsonformatter.org/json-to-jsonschema
+
+public class JSONSchemaValidation {
+
+	@Test
+	void jsonschemavalidation()
+	{
+		given()
+		
+		.when()
+			.get("http://localhost:3000/store")
+		.then()
+			.assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("storeJsonSchema.json"));
+		
+	}
+	
+	
+}
+
---
+++ src/test/java/day6/XMLSchemaValidation.java
@@ -0,0 +1,27 @@
+package day6;
+
+import org.testng.annotations.Test;
+
+import io.restassured.matcher.RestAssuredMatchers;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class XMLSchemaValidation {
+
+	@Test
+	void xmlSchemavalidation()
+	{
+		given()
+		
+		.when()
+			.get("http://restapi.adequateshop.com/api/Traveler")
+		.then()
+		.log().all()
+		   .assertThat().body(RestAssuredMatchers.matchesXsdInClasspath("traveler.xsd"));	
+		
+	}
+	
+}
